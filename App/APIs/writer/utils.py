6818d731819b4b014ed646bef08587118d09fc97
jake0109 <312519052@qq.com>
2020-03-21 21:08:33 +0800

    build some functions and decorators to verify login status.

---
+++ App/APIs/writer/utils.py
@@ -0,0 +1,43 @@
+from flask import request
+from flask_restful import abort
+
+from App.Models.writer.writer_models import Writer
+from App.extensions import cache
+
+
+def get_writer(ident):
+
+    if not ident:
+        return None
+
+    writer = Writer.query.get(ident)
+    if writer:
+        return writer
+
+    writer = Writer.query.filter(Writer.name == ident).first()
+    if writer:
+        return writer
+
+    writer = Writer.query.filter(Writer.tel == ident).first()
+    if writer:
+        return writer
+
+    writer = Writer.query.filter(Writer.mail == ident).first()
+    if writer:
+        return writer
+
+    return None
+
+def writer_login_required(func):
+    def wrapper(*args, **kwargs):
+        token = request.args.get("token")
+
+        if not token:
+            abort(400, msg="not login")
+
+        writer_id = cache.get(token)
+        if not writer_id:
+            abort(400, msg="invalid token")
+
+        return func(*args, **kwargs)
+    return wrapper
--- App/APIs/writer/writter_API.py
+++ App/APIs/writer/writter_API.py
@@ -1,7 +1,11 @@
-from flask import request
-from flask_restful import Resource, marshal, fields, marshal_with
+import uuid
 
+from flask import request
+from flask_restful import Resource, marshal, fields, marshal_with, abort
+
+from App.APIs.writer.utils import get_writer, writer_login_required
 from App.Models.writer.writer_models import Writer
+from App.extensions import cache
 
 writerFields = {
     "name": fields.String,
@@ -32,18 +36,51 @@ class writerResource(Resource):
 
     def post(self):
 
-        writer = Writer()
-        writer.name = request.form.get("name")
-        writer.password = request.form.get("password")
-        writer.mail = request.form.get("mail")
-        writer.tel = request.form.get("tel")
+        action = request.args.get("action")
 
+        if action == "register":
+            writer = Writer()
+            writer.name = request.form.get("name")
+            writer.password = request.form.get("password")
+            writer.mail = request.form.get("mail")
+            writer.tel = request.form.get("tel")
 
-        writer.save()
+            if not writer.save():
+                abort(400, msg="writer cannot be saved")
 
-        data = {
-            "msg": "successfully post",
-            "status": 201,
-            "data": marshal(writer, writerFields),
-        }
-        return data
+            data = {
+                "msg": "successfully post",
+                "status": 201,
+                "data": marshal(writer, writerFields),
+            }
+            return data
+
+        elif action == "login":
+            pass
+            ident = request.form.get("ident")
+            password = request.form.get("password")
+
+            writer = get_writer(ident)
+
+            if not writer:
+                abort(400, msg="account does not exist")
+
+            if not writer.check_password(password):
+                abort(400, msg="wrong username or password")
+
+            token = uuid.uuid4().hex
+            cache.set(token, writer.id, timeout=60 * 60 * 24 * 7)
+
+            data = {
+                "msg": "successfully login",
+                "status": 200,
+                "data": marshal(writer, writerFields),
+                "token": token
+            }
+
+            return data
+
+    @writer_login_required
+    def patch(self):
+        pass
+        return {"msg": "patch ok"}
